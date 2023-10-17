59c9a94da2d081fac9f6b9d8f33ad718abe2c1dd
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2023-10-17 15:49:12 +0300

    log

--- lua/neogit/status.lua
+++ lua/neogit/status.lua
@@ -260,7 +260,7 @@ local function draw_buffer()
 
   local function render_section(header, key, data)
     local section_config = config.values.sections[key]
-    print(vim.inspect(data))
+    -- print(vim.inspect(data))
     print("***************************************")
     print(vim.inspect(git))
     if section_config.hidden then
